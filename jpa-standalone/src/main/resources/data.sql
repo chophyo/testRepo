4babc025c333779f6a6383422a9630c05f6c7d4b
chophyo <floristflorist151@gmail.com>
2023-01-11 08:43:16 +0630

    ss

--- jpa-standalone/src/main/resources/data.sql
+++
@@ -1,8 +0,0 @@
-insert into employee(id, first_name, last_name, salary, email)
-values
-    (1,'Thaw', 'Thaw', 2000, 'thaw@gmail.com'),
-    (2,'John', 'Doe', 1000, 'john@gmail.com'),
-    (3, 'John', 'William', 3000, 'william@gmail.com'),
-    (4, 'Thomas', 'Hardy', 3000, 'thomas@gmail.com'),
-    (5, 'Sai', 'Sai', 1000, 'sai@gmail.com'),
-    (6, 'Angle', 'Chan', 5000, 'angle@gmail.com');
--- jpa-standalone/src/main/resources/schema.sql
+++
@@ -1,7 +0,0 @@
-create table employee(
-    id int not null ,
-    first_name varchar(60),
-    last_name varchar(60),
-    salary float,
-    email varchar(60)
-);
